cf59af5fb04b09465bd1cf83a6c71a43142fe3c2
csjwhats <csjwhats@gmail.com>
2019-05-26 23:20:42 +0900

    WPF xamlの設定

--- AddWpf/AddWpf/MainWindow.xaml.cs
+++ AddWpf/AddWpf/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace AddWpf
         {
             InitializeComponent();
         }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
     }
 }
